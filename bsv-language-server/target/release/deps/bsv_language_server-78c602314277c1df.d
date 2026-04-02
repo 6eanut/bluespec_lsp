88b637ceca8f6053da40fd0a695a2a2593a0168e
6eanut <jiakaiPeanut@gmail.com>
2026-04-02 19:01:32 +0800

    chore: remove tracked target directory
    The target/ directory contains Rust build artifacts and should not be
    tracked in version control. Added to .gitignore.
    
    Signed-off-by: 6eanut <jiakaiPeanut@gmail.com>

--- bsv-language-server/target/release/deps/bsv_language_server-78c602314277c1df.d
+++
@@ -1,5 +0,0 @@
-/home/jiakai/bsv-language-server/target/release/deps/bsv_language_server-78c602314277c1df.d: src/main.rs
-
-/home/jiakai/bsv-language-server/target/release/deps/bsv_language_server-78c602314277c1df: src/main.rs
-
-src/main.rs:
